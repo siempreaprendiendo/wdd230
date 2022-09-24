22413ffaf9ad216aeedee7b9639dcae37f379810
siempreaprendiendo <jim20003@byui.edu>
2022-09-23 18:44:28 -0700

    Lesson 2

--- lesson2/scripts/lesson2.js
+++ lesson2/scripts/lesson2.js
@@ -1,6 +1,5 @@
-// -------------- Date --------------
+// ----------Date and Year----------------
 
 document.getElementById("date").innerHTML = `Last Updated: ${document.lastModified}`;
 document.getElementById("year").innerText = new Date().getFullYear();
-
 
