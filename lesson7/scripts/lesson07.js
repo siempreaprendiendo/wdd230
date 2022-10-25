1397ab48cc09030640218c567b7cf6040ce2f414
siempreaprendiendo <jim20003@byui.edu>
2022-10-24 20:52:08 -0700

    lazy loading activity

---
+++ lesson7/scripts/lesson07.js
@@ -0,0 +1,34 @@
+ // Images are retrieved with data-scr attribute
+const imagesToLoad = document.querySelectorAll("img[data-src]")
+
+// Parameters being set for the IntersectionalObserver
+const imgOptions = {
+    threshold: 100,
+    rootMargin: "0px 0px -500px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+if('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      imgObserver.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
+  
