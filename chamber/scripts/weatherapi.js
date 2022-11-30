e25fa66de7077f6b8ae3e908042ede5ea93c6137
siempreaprendiendo <jim20003@byui.edu>
2022-11-30 14:44:51 -0800

    css fix

--- chamber/scripts/weatherapi.js
+++ chamber/scripts/weatherapi.js
@@ -22,7 +22,7 @@ async function apiFetch(apiURL) {
 
 function displayResults(weatherData) {
     currentTemp.innerHTML = weatherData.main.temp.toFixed(1);
-    const imagesrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const imagesrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
     const desc = weatherData.weather[0].description;
     weatherIcon.setAttribute('src', imagesrc);
     weatherIcon.setAttribute('alt', desc);
