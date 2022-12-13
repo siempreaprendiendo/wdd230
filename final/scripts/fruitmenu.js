b835986878714e9144e8a34ff0211231c00b7953
siempreaprendiendo <jim20003@byui.edu>
2022-12-13 14:38:15 -0800

    java+

--- final/scripts/fruitmenu.js
+++ final/scripts/fruitmenu.js
@@ -1,5 +1,8 @@
-const requestURL = "https://brotherblazzard.github.io/canvas-content/fruit.json";
-const fruitSelection = document.querySelector('#fruits');
+const requestURL = "https://siempreaprendiendo.github.io/wdd230/final/json/fruits.json";
+const fruitMenu1 = document.querySelector('.fruits');
+const fruitMenu2 = document.querySelector('.fruits2');
+const fruitMenu3 = document.querySelector('.fruits3');
+
 
 async function getDirectory() {
 
@@ -9,21 +12,36 @@ async function getDirectory() {
         console.log(data);
         buildList(data);
     } else {
-        throw Error(response.statusText)
+        throw Error(response.statusText);
     }
 }
 
 function buildList(data) {
     data.fruits.forEach(fruit => {
-        let menu = document.createElement('option');
-
-        menu.innerHTML = `${fruit.name}`;
-
+        let label = document.createElement('label');
+        let option = document.createElement("option");
         option.setAttribute('value', `${fruit.name}`);
-        
-        
-        
-    })
+        label.innerHTML = `${fruit.name}`;
+        option.append(label);
+        fruitMenu1.append(option);
+
+        let label2 = document.createElement('label');
+        let option2 = document.createElement("option");
+        option2.setAttribute('value', `${fruit.name}`);
+        label2.innerHTML = `${fruit.name}`;
+        option2.append(label2);
+        fruitMenu2.append(option2);
+
+        let label3 = document.createElement('label');
+        let option3 = document.createElement("option");
+        option3.setAttribute('value', `${fruit.name}`);
+        label3.innerHTML = `${fruit.name}`;
+        option3.append(label3);
+        fruitMenu3.append(option3);
+
+    });
 }
 
 getDirectory();
+
+
