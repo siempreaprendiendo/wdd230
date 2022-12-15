6d5cac3c69c90f9521fd3acafcf7fe29f7b634b1
siempreaprendiendo <jim20003@byui.edu>
2022-12-14 18:14:56 -0800

    java error

--- final/scripts/main.js
+++ final/scripts/main.js
@@ -1,3 +1,24 @@
+//  Date 
+
+document.getElementById("dateMod").innerHTML = `Last Modified: ${document.lastModified}`;
+document.getElementById("year").innerText = new Date().getFullYear();
+const dateElement = document.querySelector(".date");
+
+function formatDate(date) {
+    const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+    const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  
+    return `${DAYS[date.getDay()]}, ${
+      MONTHS[date.getMonth()]
+    } ${date.getDate()} ${date.getFullYear()}`;
+  }
+  
+  // setInterval(() => {
+  //   const now = new Date();
+  
+  //   dateElement.textContent = formatDate(now);
+  // }, 200);
+
 // Hamburger button javascript
 
 const menuClosed = document.querySelector('#menuArea');
@@ -18,23 +39,3 @@ menuButton.addEventListener('click', () => {
     }
 });
 
-//  Date 
-
-document.getElementById("dateMod").innerHTML = `Last Modified: ${document.lastModified}`;
-document.getElementById("year").innerText = new Date().getFullYear();
-const dateElement = document.querySelector(".date");
-
-function formatDate(date) {
-    const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
-    const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  
-    return `${DAYS[date.getDay()]}, ${
-      MONTHS[date.getMonth()]
-    } ${date.getDate()} ${date.getFullYear()}`;
-  }
-  
-  setInterval(() => {
-    const now = new Date();
-    dateElement.textContent = formatDate(now);
-    
-  }, 200);
