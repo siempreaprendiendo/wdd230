ea9f75b384c826219e2d05a2e67f94a5d83125e8
siempreaprendiendo <jim20003@byui.edu>
2022-12-14 17:35:05 -0800

    resource page

--- final/scripts/main.js
+++ final/scripts/main.js
@@ -17,3 +17,24 @@ menuButton.addEventListener('click', () => {
         menuOpen = false;
     }
 });
+
+//  Date 
+
+document.getElementById("dateMod").innerHTML = `Last Modified: ${document.lastModified}`;
+document.getElementById("year").innerText = new Date().getFullYear();
+const dateElement = document.querySelector(".date");
+
+function formatDate(date) {
+    const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+    const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  
+    return `${DAYS[date.getDay()]}, ${
+      MONTHS[date.getMonth()]
+    } ${date.getDate()} ${date.getFullYear()}`;
+  }
+  
+  setInterval(() => {
+    const now = new Date();
+    dateElement.textContent = formatDate(now);
+    
+  }, 200);
