caf17b809d6800e519357c5a78ecd50d689dc494
siempreaprendiendo <jim20003@byui.edu>
2022-12-15 16:15:40 -0800

    final touches

--- final/scripts/submitcount.js
+++
@@ -1,8 +0,0 @@
-var count = 0;
-        var btn = document.getElementById("subBtn");
-        var disp = document.getElementById("display");
-  
-        btn.onclick = function () {
-            count++;
-            disp.innerHTML = count;
-        }
